1af20736d1e4f7f8b26879d4772fa617d2a8e77d
Yusuf Qisthi <yqisthi2002@gmail.com>
2022-11-30 19:38:00 +0700

    phLevelFragmentViewModel Fix

--- app/src/main/java/com/kedaireka/monitoringkjabb/ui/statistics/parameter/PhLevelsFragment.kt
+++ app/src/main/java/com/kedaireka/monitoringkjabb/ui/statistics/parameter/PhLevelsFragment.kt
@@ -5,13 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.kedaireka.monitoringkjabb.R
 import com.kedaireka.monitoringkjabb.databinding.FragmentPhLevelsBinding
 import com.kedaireka.monitoringkjabb.model.Sensor
 
 
 class PhLevelsFragment : Fragment() {
-    private lateinit var phLvelFragmentViewModel: AmmoniaFragmentViewModel
+    private lateinit var phLvelFragmentViewModel: PhLevelsFragmentViewModel
     private lateinit var recordsInRange: ArrayList<Sensor>
 
     private var _binding: FragmentPhLevelsBinding? = null
@@ -20,4 +21,15 @@ class PhLevelsFragment : Fragment() {
     private var max = 0.0
     private var min = 0.0
     private var avg = 0.0
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        phLvelFragmentViewModel = ViewModelProvider(this)[PhLevelsFragmentViewModel::class.java]
+
+        _binding = FragmentPhLevelsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+    }
 }
